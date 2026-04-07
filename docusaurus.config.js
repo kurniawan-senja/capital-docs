68d679db48ca0cca2dce456e122cf5801a433d62
kurniawan-senja <email.kurniawan@domain.com>
2026-04-07 19:57:26 +0700

    fix(config): resolve onBrokenMarkdownLinks deprecation warning

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,12 @@ const config = {
   projectName: 'capital-docs',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   i18n: {
     defaultLocale: 'en',
